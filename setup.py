910a6408bd74139165dc2b8f00e71701f62e7dd7
Neraverin <neraverin@gmail.com>
2021-03-19 15:20:21 +0300

    Fix some warnings, add requirements in setup.py and bump version

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
+# Setup file
 from setuptools import setup
 
 setup(
-    name='wallarm_api',
     packages=['wallarm_api'],
-    version='0.5',
+    name='wallarm_api',
+    version='0.6',
     license='Apache license 2.0',
     description='Wallarm API client',
     author='Vladimir Salykin',
     author_email='neraverin@gmail.com',
     url='https://github.com/Neraverin/wallarm-api-python',
-    download_url='https://github.com/Neraverin/wallarm-api-python/archive/v0.5.tar.gz',
+    download_url='https://github.com/Neraverin/wallarm-api-python/archive/v0.6.tar.gz',
     keywords=['Wallarm', 'API client'],
     install_requires=[
         'requests',
+        'pydantic',
+        'urllib3'
     ],
     setup_requires=['wheel'],
     python_requires='>=3.6, <4',
--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from wallarm_api import WallarmAPI
 
 
 def get_env():
-    api_UUID = os.environ.get('WALLARM_UUID')
-    api_secret = os.environ.get('WALLARM_SECRET')
-    return api_UUID, api_secret
+    uuid = os.environ.get('WALLARM_UUID')
+    secret = os.environ.get('WALLARM_SECRET')
+    return uuid, secret
 
 
 if __name__ == '__main__':
@@ -16,4 +16,3 @@ if __name__ == '__main__':
     print(f'client_id = ', client_id)
     print(f'subscription = ', api.billing_api.get_subscription(client_id))
     print(f'dashboard_state = ', api.graph_api.get_dashboard_state(client_id))
-
